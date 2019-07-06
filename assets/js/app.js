69530cf07c52b024d75990b0b1b7e70ac2db61f8
Santiago Enciso <whereispolaris@gmail.com>
2019-07-06 11:03:47 -0500

    Added firebase credentials.

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCW9_7VxmemvQ1Znm-uy6qD7_LTf2iz-3g",
+    authDomain: "employee-data-management-7afbb.firebaseapp.com",
+    databaseURL: "https://employee-data-management-7afbb.firebaseio.com",
+    projectId: "employee-data-management-7afbb",
+    storageBucket: "",
+    messagingSenderId: "468777354917",
+    appId: "1:468777354917:web:69bf1701aa1ecd60"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
