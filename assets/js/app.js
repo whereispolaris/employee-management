6a2d131142b4129916b083a4c93f547c59ab7f25
Santiago Enciso <whereispolaris@gmail.com>
2019-07-06 11:45:08 -0500

    Added onsubmit event handler.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyCW9_7VxmemvQ1Znm-uy6qD7_LTf2iz-3g",
     authDomain: "employee-data-management-7afbb.firebaseapp.com",
     databaseURL: "https://employee-data-management-7afbb.firebaseio.com",
@@ -8,22 +8,28 @@ var firebaseConfig = {
     messagingSenderId: "468777354917",
     appId: "1:468777354917:web:69bf1701aa1ecd60"
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+
+firebase.initializeApp(config);
 
 // Grab variables from form fields
 var database = firebase.database();
 
-var empName = $("#empName").val().trim();
-console.log(empName);
-var empRole = $("#empRole").val().trim();
-var monthRate = $("#monthRate").val().trim();
-var startDate = $("#startDate").val().trim();
+$("#addEmployee").on("submit", function (event) {
+    event.preventDefault();
 
-//push values intot database  
-database.ref().push({
-    employee: empName,
-    role: empRole,
-    rate: monthRate,
-    date: startDate,
-});
+
+
+
+    var empName = $("#empName").val().trim();
+    var empRole = $("#empRole").val().trim();
+    var monthRate = $("#monthRate").val().trim();
+    var startDate = $("#startDate").val().trim();
+
+    //push values intot database  
+    database.ref().push({
+        employee: empName,
+        role: empRole,
+        rate: monthRate,
+        date: startDate,
+    });
+})
